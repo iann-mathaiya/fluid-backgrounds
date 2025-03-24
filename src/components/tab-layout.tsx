64d9c7aa64f191afb2daf4828c2b3b8b18ce9fd7
Ian Mathaiya <iannmathaiya@gmail.com>
2025-03-24 21:15:22 +0300

    deps: add tw merge

--- src/components/tab-layout.tsx
+++ src/components/tab-layout.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { TABS } from '../lib/constants';
+import { twMerge } from 'tailwind-merge';
 
 export default function TabLayout() {
     const [activeTab, setActiveTab] = useState(TABS[0]);
@@ -9,7 +10,11 @@ export default function TabLayout() {
             <ul className="mt-8 sm:mt-12 flex items-center gap-2">
                 {TABS.map(tab =>
                     <li key={tab}>
-                        <button type='button' className="py-1.5 px-4 size-fit text-sm text-zinc-600 hover:text-zinc-900 bg-zinc-200 rounded-full">
+                        <button type='button' 
+                            className={twMerge(
+                                "py-1.5 px-4 size-fit text-sm text-zinc-600 hover:text-zinc-900 bg-transparent rounded-full",
+                                activeTab === tab && "text-white bg-zinc-900"
+                            )}>
                             {tab}
                         </button>
                     </li>
