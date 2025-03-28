d517e9ab67150ba30b19fe7e1c0ba9ad37182115
Ian Mathaiya <iannmathaiya@gmail.com>
2025-03-28 20:55:37 +0300

    update warp bg component

--- src/components/warp-background.tsx
+++ src/components/warp-background.tsx
@@ -2,13 +2,14 @@ import { Warp, type WarpProps } from '@paper-design/shaders-react';
 
 type MeshBackgroundProps = WarpProps
 
-export default function WarpBackground({ color1, color2, color3, speed = 0.4 }: MeshBackgroundProps) {
+export default function WarpBackground({ color1, color2, color3, speed = 0.4, rotation= 0.5 }: MeshBackgroundProps) {
     return (
         <Warp
             color1={color1}
             color2={color2}
             color3={color3}
             speed={speed}
+            rotation={rotation}
             style={{ width: '100%', height: '100%' }}
         />
     );
