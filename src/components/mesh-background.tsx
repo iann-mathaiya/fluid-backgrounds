318ce66734ccdcc961de24cfac4bc539bc660644
Ian Mathaiya <iannmathaiya@gmail.com>
2025-03-29 08:27:39 +0300

    update mesh bg props

--- src/components/mesh-background.tsx
+++ src/components/mesh-background.tsx
@@ -1,22 +1,11 @@
-import { MeshGradient } from '@paper-design/shaders-react';
+import { MeshGradient, type MeshGradientProps } from '@paper-design/shaders-react';
 
-type MeshBackgroundProps = {
-    color1: string;
-    color2: string;
-    color3?: string;
-    color4?: string;
-    speed?: number;
-}
+export default function MeshBackground(props: MeshGradientProps) {
 
-export default function MeshBackground({ color1, color2, color3, color4, speed = 0.4 }: MeshBackgroundProps) {
-    return (
-        <MeshGradient
-            color1={color1}
-            color2={color2}
-            color3={color3}
-            color4={color4}
-            speed={speed}
-            style={{ width: '100%', height: '100%' }}
-        />
-    );
+    const defaultProps = {
+        speed: 0.4,
+        style: { width: '100%', height: '100%' }
+    };
+
+    return <MeshGradient {...defaultProps} {...props} style={{ ...defaultProps.style, ...props.style }} />
 }
